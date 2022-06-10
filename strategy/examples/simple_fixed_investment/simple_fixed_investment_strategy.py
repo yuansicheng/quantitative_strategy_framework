73777c618e1822278cd695a6e38e1b3b5405374b
yuansc <yuansc23@outlook.com>
2022-06-10 10:47:19 +0800

    Migrate some strategies

--- strategy/examples/simple_fixed_investment/simple_fixed_investment_strategy.py
+++ strategy/examples/simple_fixed_investment/simple_fixed_investment_strategy.py
@@ -23,7 +23,7 @@ class SimpleFixedInvestment(Strategy):
         self.profit_stop = self.strategy_args['profit_stop']
         self.sell_proportion = self.strategy_args['sell_proportion']
 
-        self.last_sell_weeks = {asset: -1 for asset in self.asset_list}
+        self.last_sell_days = {asset: -1 for asset in self.asset_list}
         
 
     def backtestOneDay(self):
@@ -33,9 +33,9 @@ class SimpleFixedInvestment(Strategy):
         # 20220520 update: self.user_asset_ages were added to user-data
 
         # update sell dict
-        for asset in self.last_sell_weeks:
-            if self.last_sell_weeks[asset] != -1:
-                self.last_sell_weeks[asset] += 1
+        for asset in self.last_sell_days:
+            if self.last_sell_days[asset] != -1:
+                self.last_sell_days[asset] += 1
         
 
         if self.current_date in self.update_date:
@@ -50,7 +50,7 @@ class SimpleFixedInvestment(Strategy):
             # print(self.current_date, asset, self.asset_positions[asset].total_yield, self.last_sell_weeks[asset])
             if self.asset_positions[asset].current_yield > self.profit_stop and (self.last_sell_weeks[asset] > self.constants['DAY_OF_MONTH'] or self.last_sell_weeks[asset] == -1):
                 self.orders.append(Order(date=self.current_date, asset_name=asset, money=-self.sell_proportion * self.asset_positions[asset].position, mark='Achieve target profit'))
-                self.last_sell_weeks[asset] = 0
+                self.last_sell_days[asset] = 0
             else:
                 self.orders.append(Order(date=self.current_date, asset_name=asset, money=self.fixed_investment, mark='Fixedinvestment'))
 
